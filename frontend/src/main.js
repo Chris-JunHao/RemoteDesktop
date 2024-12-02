2d5c130b391a97bc172e595a64cbc72c7c6a5935
Chris <jh2265339886@gmail.com>
2024-12-02 12:22:48 +0800

    删除前后端

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';  // 确保正确导入路由器
-
-const app = createApp(App);
-app.use(router); // 使用路由器
-app.mount('#app'); // 挂载应用到 DOM
